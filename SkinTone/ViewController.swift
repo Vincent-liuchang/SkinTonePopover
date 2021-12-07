51374f8fba6f21ab2589515df31ffd299f80f0de
ChangLiu <viliu@cisco.com>
2021-12-07 15:18:18 +0800

    Initial Commit

---
+++ SkinTone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkinTone
+//
+//  Created by ChangLiu on 2021/12/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
